6dea0112bfdfa37342089c3484f62a1745626c4e
Josue Araya <jdag98228@gmail.com>
2019-09-20 06:58:16 -0600

    Server final

--- GreetServer.java
+++ GreetServer.java
@@ -12,22 +12,24 @@ public class GreetServer {
 
 
     public void start(int port) throws IOException {
-        serverSocket = new ServerSocket(port);
+    	InetAddress ip = InetAddress.getByName("192.168.43.209");
+        serverSocket = new ServerSocket(port,1,ip);
         clientSocket = serverSocket.accept();
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-     
+        System.out.println("Perro1");
 
         String inputLine;
         while (true) {
         	System.out.println("Existen dos tipos de enemigos, rojos y azules");
-        	System.out.println("Para enviar un enemigo Rojo digite R seguido de el numero de liana donde lo quiera colocar");
-        	System.out.println("Ejemplo: R9");
+        	System.out.println("Para enviar un enemigo Rojo digite R seguido de el numero de liana donde lo quiera colocar, use numeros de dos digitos del 00 al 11");
+        	System.out.println("Ejemplo: R09");
         	System.out.println("Para enviar un enemigo Azul siga el mismo procedimiento pero empezando con la A y luego el numero de liana");
             System.out.print("Enemigo: ");
             String response = scanner.nextLine();
             if (validateString(response)){
-            	out.println(response);
+            	String finalresponse = response.substring(0,1) + "&" +response.substring(1);
+            	out.println(finalresponse);
             }
             else {
             	System.out.println("Error en codigo de enemigo, intentelo de nuevo");
@@ -37,19 +39,21 @@ public class GreetServer {
     }
     
     public boolean validateString(String response) {
-    	if (response.length()== 2) {
-    		if (("R").equals(response.substring(1)) || ("A").equals(response.substring(1))) {
+    	if (response.length()== 3) {
+    		if (("R").equals(response.substring(0,1)) || ("A").equals(response.substring(0,1))) {
     			switch(response.substring(1)){
-    			case "0": return true;
-    			case "1": return true;
-    			case "2": return true;
-    			case "3": return true;
-    			case "4": return true;
-    			case "5": return true;
-    			case "6": return true;
-    			case "7": return true;
-    			case "8": return true;
-    			case "9": return true;
+    			case "00": return true;
+    			case "01": return true;
+    			case "02": return true;
+    			case "03": return true;
+    			case "04": return true;
+    			case "05": return true;
+    			case "06": return true;
+    			case "07": return true;
+    			case "08": return true;
+    			case "09": return true;
+    			case "10": return true;
+    			case "11": return true;
     			default: return false;
     			}
     		}
